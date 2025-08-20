601e1f65b44be1f69db9b89894969749be74983c
Adrian Fernandes <boybothere@gmail.com>
2025-08-20 09:41:02 +0530

    feat: Initial user creation and MongoDB setup

---
+++ src/users/dto/create-user.dto.ts
@@ -0,0 +1,12 @@
+import { IsNotEmpty, IsOptional, IsString } from "class-validator";
+
+export class CreateUserDto {
+
+    @IsNotEmpty()
+    @IsString()
+    username: string;
+
+    @IsOptional()
+    @IsString()
+    displayName?: string;
+}
