7120376140a38c371982e3564218b67d071e5fb6
Adrian Fernandes <boybothere@gmail.com>
2025-08-20 11:11:49 +0530

    feat: Implement user CRUD

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,6 +1,7 @@
-import { Body, Controller, Post, UsePipes, ValidationPipe } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Patch, Post, UsePipes, ValidationPipe } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
 
 @Controller('users')
 export class UsersController {
@@ -12,5 +13,25 @@ export class UsersController {
     createUser(@Body() createUserDto: CreateUserDto) {
         console.log(createUserDto)
         return this.usersService.createUser(createUserDto)
+    }
+
+    @Get()
+    getUsers() {
+        return this.usersService.getAllUsers()
+    }
+
+    @Get(':id')
+    getUserById(@Param('id') id: string) {
+        return this.usersService.getUserById(id)
+    }
+
+    @Patch(':id')
+    @UsePipes(new ValidationPipe())
+    updateUser(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
+        return this.usersService.updateUser(id, updateUserDto)
+    }
+    @Delete(':id')
+    deleteUser(@Param('id') id: string) {
+        return this.usersService.deleteUser(id)
     }
 }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,15 +1,41 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { User } from 'src/schemas/User.schema';
 import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
 
 @Injectable()
 export class UsersService {
     constructor(@InjectModel(User.name) private readonly userModel: Model<User>) { }
 
-    createUser(createUserDto: CreateUserDto) {
+    async createUser(createUserDto: CreateUserDto) {
         const newUser = new this.userModel(createUserDto)
-        return newUser.save()
+        await newUser.save()
+        return newUser
+    }
+
+    getAllUsers() {
+        return this.userModel.find()
+    }
+
+    async getUserById(id: string) {
+        const user = await this.userModel.findById(id)
+        if (!user) throw new NotFoundException('User not found!')
+        return user
+    }
+
+    async updateUser(id: string, updateUserDto: UpdateUserDto) {
+        const findUser = await this.userModel.findById(id)
+        if (!findUser) throw new NotFoundException("User not found, invalid request!")
+        await this.userModel.findByIdAndUpdate(id, updateUserDto)
+        return { message: "User updated successfully" }
+    }
+
+    async deleteUser(id: string) {
+        const findUser = await this.userModel.findById(id)
+        if (!findUser) throw new NotFoundException("User not found, invalid request!")
+        await this.userModel.findByIdAndDelete(id)
+        return { message: "User deleted successfully" }
     }
 }
